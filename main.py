1603470c8d64d19486155a173d23a67c89f4daa5
nikmaltcev <128372756+nikmaltcev@users.noreply.github.com>
2023-07-01 18:01:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('6006820261:AAHYfSGzvpc9eFojQa91S1rTukq6OYLDRrQ')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://b420156.yclients.com/')))
+    await message.answer('Привет, мой друг!', reply_markup=markup)
+
+executor.start_polling(dp)
